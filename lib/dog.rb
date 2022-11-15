bb561b5d098b2330082903e54ff097cf7390238a
Josh Burd <Jburdt@gmail.com>
2022-11-15 17:53:38 -0500

    commit tree 518ba647acb8b5e11cd1ae8d08a58a9959a474d1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
 
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
 end
+
+fido = Dog.new()
+fido.name = "Fido"
+
+snoopy = Dog.new
+snoopy.breed = "Breagle"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
+    def name=(name)
+        @name = name
+    end
 
+    def job=(job)
+        @job = job
+    end
 end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+
+beyonce.job = "Singer"
